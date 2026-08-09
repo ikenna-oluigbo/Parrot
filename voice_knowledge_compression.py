4db87babe0f71df5b515a9a047ab0b27b6159bc2
Ikenna Oluigbo <64980885+ikenna-oluigbo@users.noreply.github.com>
2026-08-09 23:48:57 +0200

    Refactor extract_relevant_doc for API key usage
    Updated API key handling in extract_relevant_doc function.

--- voice_knowledge_compression.py
+++ voice_knowledge_compression.py
@@ -2,20 +2,17 @@ from langchain_classic.retrievers import ContextualCompressionRetriever
 from langchain_classic.retrievers.document_compressors import LLMChainExtractor 
 from langchain_openai import OpenAI
 
-from ap_keys import my_api_key
-OPENAI_API_KEY, CORE_API_KEY, ELEVENLABS_API_KEY, GEMINI_API_KEY, COHERE_API_KEY = my_api_key()
-
 def pretty_print_docs(docs):
     return f"\n{'-' * 100}\n".join([f"Document {i+1}:\n\n" + d.page_content for i, d in enumerate(docs)])
 
 def extract_relevant_doc(vector_db, question):
     # Wrap our vectorstore
-    llm = OpenAI(api_key=OPENAI_API_KEY, model="gpt-5.4", temperature=0.1)
+    llm = OpenAI(api_key=<ADD YOUR OPENAI_API_KEY>, model="gpt-5.4", temperature=0.1)
     compressor = LLMChainExtractor.from_llm(llm) 
 
     compression_retriever = ContextualCompressionRetriever(
         base_compressor=compressor,
-        base_retriever=vector_db.as_retriever(search_type = "mmr")  #base_retriever=vector_db.as_retriever()
+        base_retriever=vector_db.as_retriever(search_type = "mmr")  
     )
 
     compressed_docs = compression_retriever.invoke(question)        
